e25bc880cc3f8730b6306d66b667ee28589bde94
Logan Pihl <logan@LP-MBA.example.com>
2020-02-15 23:18:56 -0800

    webpage setup done for now. coding/brainstorming ensues

--- code.js
+++ code.js
@@ -43,22 +43,40 @@
 // ----------------------------------DEFINITIONS----------------------------------
 let price_coeff = 1.12;
 let prestige = true;
+let transUnlock = false;
 // insert if statement here when necessary (prestige affecting price coefficients)
+//------------------------------------RESOURCES-----------------------------------
+//population
+let cultists = 0;
+let cultistCap = 0;
+//basic materials
+let wood = 10;
+let praise = 0;
 // -----------------------------------BUILDINGS-----------------------------------
 let temple = {
   name: 'temple',
   price: 10,
   amount: 0,
   desc: 'a basic gathering ground for cult members',
-  effect: 'generates .5 praise every second',
+  effect: 'gives a cultist slot per level, allowing for 0.5 praise per second',
 };
-
 function updateBuilding(x) {
 	if (x==1) {
     	temple.amount++;
-      let tempPrice = Math.round(1000*(Math.pow(price_coeff,temple.amount)));
-    	temple.price = tempPrice/100;
+      cultistCap++;
+      if (temple.amount>=1) {
+        document.getElementById("cultists").hidden = false;
+        document.getElementById("cultists").innerHTML = "cultists: " + cultists + "/" + cultistCap;
+      };
+    	temple.price = (Math.round(1000*(Math.pow(price_coeff,temple.amount*temple.amount))))/100;
 	};
+};
+
+function purchaseBuilding(x) {
+  if (x==1 && wood>=temple.price) {
+    wood = wood-temple.price;
+    updateBuilding(x);
+  };
 };
 /* pseudocode:
 if user clicks on temple buy button AND user has enough resources:
@@ -68,3 +86,68 @@ elif user clicks on buy AND user doesn't have enough resources:
 
 // -------------------------------MAIN MENU FUNCTION------------------------------
 // Figure out how to code between JS/HTML
+// ----------------------------------TAB FUNCTIONS---------------------------------
+function openManageTab(evt, tabName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("managetabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("managetablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+  //ty w3schools
+}
+
+function openGameTab(evt, tabName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("gametabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("gametablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+  //ty w3schools
+}
+
+// ---------------------------------TICK FUNCTIONS--------------------------------
+window.setInterval(function() {
+  if (transUnlock==false) {
+    if (praise>=10) {
+      transUnlock=true;
+      document.getElementById("transTab").hidden = false;
+    };
+  };
+  if (cultists<cultistCap) {
+    if (Math.random()<=.05) {
+      cultists++;
+      document.getElementById("cultists").innerHTML = "cultists: " + cultists + "/" + cultistCap;
+    };
+  };
+  document.getElementById("wood").innerHTML = "wood: " + wood;
+  tempPraise = praise+(cultists)
+  praise = (Math.round(100*(praise+(cultists*.5)/10)))/100;
+  document.getElementById("praise").innerHTML = "praise: " + praise;
+  console.log(wood + " wood, " + praise + " praise");
+},100);
